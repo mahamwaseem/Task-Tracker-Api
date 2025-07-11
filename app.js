fabf6a196cdbaa68a1b8162a07e269fcf91f2c94
Maham Waseem <mahamw440@gmail.com>
2025-07-11 11:46:38 +0500

    setup mongoose connection

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+app.use(express.json());
+
+// MongoDB Connection
+mongoose.connect('mongodb://127.0.0.1:27017/tasktracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
+
+// Routes
+app.use('/tasks', taskRoutes);
+
+// Server start
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)
+);
